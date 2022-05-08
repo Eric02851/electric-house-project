64a6751d5c423c8ca31da13d13eaf43018b8e4ba
Eric02851 <ziggy2851@gmail.com>
2022-05-08 01:16:13 -0700

    Relay 1 and 2

--- src/App.js
+++ src/App.js
@@ -59,46 +59,63 @@ class ColorButtonList extends React.Component {
                 <ColorButton on={this.state['yellow']} color='yellow' onClick={this.onClick} onUnclick={this.onUnclick} />
                 <ColorButton on={this.state['green']} color='green' onClick={this.onClick} onUnclick={this.onUnclick} />
                 <ColorButton on={this.state['blue']} color='blue' onClick={this.onClick} onUnclick={this.onUnclick} />
-                <ColorButton on={this.state['cyan']} color='cyan' onClick={this.onClick} onUnclick={this.onUnclick} />
                 <ColorButton on={this.state['purple']} color='purple' onClick={this.onClick} onUnclick={this.onUnclick} />
             </div>
         )
     }
 }
-class RgbRelay extends React.Component {
+
+class Relay2 extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { on: false };
+        this.state = {on: false, style: {color: 'red', backgroundColor: 'black'}}
     }
 
-    onOff = () => {
-        if (!this.state.on) this.setState({ on: true })
-        else this.setState({ on: false })
+    onClick = () => {
+        if (!this.state.on) this.setState({on: true, style: {color: 'black', backgroundColor: 'red'}})
+        else this.setState({on: false, style: {color: 'red', backgroundColor: 'black'}})
     }
 
     render() {
-        if (!this.state.on) {
+        return <button type="button" style={this.state.style} onClick={this.onClick}>Relay 2</button>
+    }
+}
+
+class Relay1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {on: false, style: {color: 'red', backgroundColor: 'black'}}
+    }
+
+    onClick = () => {
+        if (!this.state.on) this.setState({on: true, style: {color: 'black', backgroundColor: 'red'}})
+        else this.setState({on: false, style: {color: 'red', backgroundColor: 'black'}})
+    }
+
+    render() {
+        if (!this.state.on) 
             return (
                 <div>
-                    <button type="button" onClick={this.onOff}>Click Me!</button>
-                    <h1>Off</h1>
+                    <button type="button" style={this.state.style} onClick={this.onClick}>Relay 1</button>
+                    <Relay2 />
                 </div>
             )
-        }
-
+        
         return (
             <div>
-                <button type="button" onClick={this.onOff}>Click Me!</button>
+                <button type="button" style={this.state.style} onClick={this.onClick}>Relay 1</button>
+                <Relay2 />
                 <h1>LED 1:</h1>
-                <ColorButtonList />
-                <h1>On</h1>
+                    <ColorButtonList />
+                <h1>LED 2:</h1>
+                    <ColorButtonList />
             </div>
         )
     }
 }
 
 function App() {
-    return (<RgbRelay />)
+    return (<Relay1 />)
 }
 
 export default App
